7b634ff9a1ba8d6dd6c1331f527aa8894b40e15c
Zsolti <szazsolti10@gmail.com>
2018-11-13 15:10:45 +0200

    Login+register összetéve.

--- app/src/main/java/com/example/killersolyom/projekt/LoginActivity.java
+++ app/src/main/java/com/example/killersolyom/projekt/LoginActivity.java
@@ -1,13 +1,72 @@
 package com.example.killersolyom.projekt;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthSettings;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView have_account;
+    Button login;
+    EditText phone_PhoneNum;
+    String TAG = "TAG_LOGIN";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        have_account = findViewById(R.id.have_account);
+        login = findViewById(R.id.btn_signIn);
+        phone_PhoneNum = findViewById(R.id.phone_PhoneNum);
+
+        have_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        phone_PhoneNum,        // Phone number to verify
+                        60,                 // Timeout duration
+                        TimeUnit.SECONDS,   // Unit of timeout
+                        this,               // Activity (for callback binding)
+                        mCallbacks);        // OnVerificationStateChangedCallbacks*/
+
+
+            }
+        });
+
+
+
+
     }
 }
--- app/src/main/java/com/example/killersolyom/projekt/MainActivity.java
+++ app/src/main/java/com/example/killersolyom/projekt/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.killersolyom.projekt;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-
-import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView transparent_logo;
     Animation fadein;
     Animation fadeout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,11 +44,50 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
+                //finish();
                 }
 
         };
         animation.start();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Thread animation = new Thread(){
+            @Override
+            public void run() {
+                full_logo = findViewById(R.id.logo_View2);
+                transparent_logo = findViewById(R.id.logo_View);
 
+                fadein = AnimationUtils.loadAnimation(MainActivity.this,R.anim.fadein);
+                fadeout = AnimationUtils.loadAnimation(MainActivity.this,R.anim.fadeout);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        full_logo.startAnimation(fadein);
+                    }
+                });
+
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                //finish();
+            }
+
+        };
+        animation.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
 }
