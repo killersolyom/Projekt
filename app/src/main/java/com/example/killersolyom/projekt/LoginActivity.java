c433d3cd6f6a2e074f715442dca38019bf3d4990
Zsolti <szazsolti10@gmail.com>
2018-11-13 16:53:05 +0200

    Login megvalositva, crash tortenik.

--- app/src/main/java/com/example/killersolyom/projekt/LoginActivity.java
+++ app/src/main/java/com/example/killersolyom/projekt/LoginActivity.java
@@ -1,18 +1,23 @@
 package com.example.killersolyom.projekt;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.FirebaseTooManyRequestsException;
 import com.google.firebase.auth.AuthResult;
@@ -31,15 +36,58 @@ public class LoginActivity extends AppCompatActivity {
     Button login;
     EditText phone_PhoneNum;
     String TAG = "TAG_LOGIN";
+    AlertDialog.Builder builder;
+    EditText editCode;
+
+    private String verificationId;
+    private FirebaseAuth mAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        builder = new AlertDialog.Builder(LoginActivity.this);
+
+        builder.setTitle("Írja be a kódot");
+
+        LayoutInflater inflater = getLayoutInflater();
+        View dialoglayout = inflater.inflate(R.layout.custom_aleartdialog, null);
+
+
+
+        builder.setView(dialoglayout);
+        builder.create();
+        builder.setPositiveButton("Check", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Toast.makeText(LoginActivity.this,"Get Started!",Toast.LENGTH_LONG).show();
+                editCode = findViewById(R.id.codeEdit);
+                String code = editCode.getText().toString();
+
+                if(code.isEmpty() || code.length() <6){
+                    editCode.setError("Írja be a kódot!");
+                    editCode.requestFocus();
+                    return;
+                }
+                verifyCode(code);
+                //dialog.dismiss();
+            }
+        });
+
+
+
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+
         have_account = findViewById(R.id.have_account);
         login = findViewById(R.id.btn_signIn);
         phone_PhoneNum = findViewById(R.id.phone_PhoneNum);
+
+
 
         have_account.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +102,20 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String number = phone_PhoneNum.getText().toString().trim();
+                if(number.isEmpty() || number.length()<10){
+                    phone_PhoneNum.setError("Nem érvényes telefonszám");
+                    phone_PhoneNum.requestFocus();
+                    return;
+                }
+
+                sendVerificationCode(number);
+
+                builder.show();
+
+
+                //verifyCode(code);
+
                 /*PhoneAuthProvider.getInstance().verifyPhoneNumber(
                         phone_PhoneNum,        // Phone number to verify
                         60,                 // Timeout duration
@@ -69,4 +131,53 @@ public class LoginActivity extends AppCompatActivity {
 
 
     }
+
+    private void verifyCode(String code){
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId,code);
+        signInWithCredential(credential);
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //Intent intent = new Intent(LoginActivity.this,ProfileActivity.class);
+                            //intent().setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            //startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void sendVerificationCode(String number){
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(number,60,TimeUnit.SECONDS,TaskExecutors.MAIN_THREAD,mCallBack);
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallBack
+            = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verificationId = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+            if(code != null){
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(LoginActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+    };
 }
