d3860dc6fd4558abdbef8872b1f75f2e7e7ed8ba
killersolyom <killersolyom@gmail.com>
2018-12-07 15:43:44 +0200

    deleted unused parts

--- app/src/main/java/com/example/killersolyom/projekt/ProfileFragment.java
+++ app/src/main/java/com/example/killersolyom/projekt/ProfileFragment.java
@@ -347,7 +347,6 @@ public class ProfileFragment extends Fragment {
     }
 
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
